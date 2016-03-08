578bcc183f2bd16ca3c6a56e57c36c613109ec74
Masahiro Iuchi <iuchi@wovn.io>
2016-03-08 18:09:03 +0900

    Replace with gin application.

--- application.go
+++ application.go
@@ -1,15 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":5000", nil)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":5000")
 }
